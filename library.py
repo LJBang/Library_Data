bcb6a6cbc509890cfa29687b2f34f6c4fbc7bcb7
LJBang <kailo123457@gmail.com>
2020-09-01 16:51:34 +0900

    0901 wordcloud

--- library.py
+++ library.py
@@ -2,29 +2,43 @@ import csv
 import requests
 import xmltodict
 import re
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
-f=open('C:/Users/user/Desktop/데이터 분석/library/Best2019.csv','r',encoding='cp949') #파일 주소
+f=open('./dataset/Best2017.csv','r',encoding='cp949') #파일 주소
 data=csv.reader(f)
 header=next(data)
 cnt=0
-url='http://data4library.kr/api/keywordList?authKey=35c3bfc58502e41b21e1c4ac301a8056cbf1902fccd78698b85a20c2bbbb3fdb&isbn13='
+authk = ''
+url='http://data4library.kr/api/keywordList?authKey='+authk+'&isbn13='
 result='result/' #결과 저장할 폴더
+k=0
+keyword_list=""
+
 for row in data:
+    if k>99:
+        break
     new_url=url+row[6]
-    new_file_name=re.sub('[:-?%*|"<>./]','',row[1]) #엑셀 제목에 못 쓰는 특수문자 제거
-    new_file_name=result+new_file_name.strip()+'.csv' #공백 제거
-    #print(new_file_name)
+#    new_file_name=re.sub('[:-?%*|"<>./]','',row[1]) #엑셀 제목에 못 쓰는 특수문자 제거
+#    new_file_name=result+new_file_name.strip()+'.csv' #공백 제거
+#    print(new_file_name)
     req=requests.get(new_url).content
     xmlObject=xmltodict.parse(req)
     keyword=xmlObject['response']['items']
     if keyword: #키워드가 있는 경우
-        nf=open(new_file_name,'w',newline='')
-        write=csv.DictWriter(nf,fieldnames=['키워드','빈도수'])
-        write.writeheader()
         keyword=xmlObject['response']['items']['item']
         for i in range(len(keyword)):
-            write.writerow({'키워드':keyword[i]['word'],'빈도수':keyword[i]['weight']})
+            if keyword[i]['weight']!='1':
+                keyword_list=keyword_list+' '+keyword[i]['word']
     else: #키워드가 없는 경우
         print(row[1]+'has no keyword')
-    nf.close
+    k = k + 1
+
+wc = WordCloud(font_path='fonts/GmarketSansTTFBold.ttf', background_color='white').generate(keyword_list)
+plt.figure(figsize=(22,22)) #이미지 사이즈 지정
+plt.imshow(wc, interpolation='lanczos') #이미지의 부드럽기 정도
+plt.axis('off') #x y 축 숫자 제거
+plt.savefig('./images/2017.png')
+plt.show()
+
 f.close
